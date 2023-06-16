cbbdc44a5cb9574fa9cd1c8d8945eb1b2822bc70
fontain richardson <fontainwr@gmail.com>
2023-06-16 17:31:02 +0900

    woking on forms, edit, create, comments section with html front

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_14_070729) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_16_065516) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
